d29d942be64ce99a16aa2924cac8d818fcedd40a
Tyler <insomniac34@gmail.com>
2014-09-30 17:04:17 -0400

    Enhanced randomized word testing to guarantee unique word.

--- DLBTest.java
+++ DLBTest.java
@@ -11,17 +11,23 @@ import static org.junit.Assert.*;
 
 public class DLBTest {
 
+    //DLB output values for easier referencing
+    private final int IS_WORD_ONLY = 2;
+    private final int IS_PREFIX_AND_WORD = 3;
+    private final int IS_PREFIX_ONLY = 1;
+    private final int NOT_WORD_OR_PREFIX = 0;
+
     private final int WORD_COUNT = 10;
     private final int WORD_SIZE = 10;
     private final String[] testWords = {
-                                   "This is a test",
+                                   "test",
                                    "word",
                                    "blah",
                                    "stuff",
                                    "things"
                                    };
 
-    private char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'};
+    private final char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'};
 
     @Test
     //DLB.searchPrefix(StringBuilder s) should return int 1 if a searched word is a prefix
@@ -52,36 +58,46 @@ public class DLBTest {
     @Test
     public void testDLBWordCount() {
 
-
     }
 
     @Test
     public void testRandomizedWordInsertion() {
         DLB theDLB = new DLB();
-
         Random rnd = new Random();
         rnd.setSeed(0);
-        String[] randomStrings = new String[this.WORD_COUNT];
-
+        ArrayList<String> randomStrings = new ArrayList<String>(this.WORD_COUNT);
         for (int i = 0; i < this.WORD_COUNT; i++) {
             char[] chars = new char[this.WORD_SIZE];
             for (int j = 0; j < this.WORD_SIZE; j++) {
                 int randomLetterIndex = rnd.nextInt(this.alphabet.length);
                 chars[j] = this.alphabet[randomLetterIndex];
             }
-            randomStrings[i] = new String(chars);
+
+            String newString = (randomStrings.contains(new String(chars))) ? null : new String(chars);
+            if (newString == null) {
+               String altString = randomStrings.get(0);
+               char[] altChars = new char[this.WORD_COUNT];
+               while (randomStrings.contains(altString)) {
+                   for (int j = 0; j < this.WORD_SIZE; j++) {
+                       int altRandom = this.alphabet[rnd.nextInt(this.alphabet.length)];
+                       altChars[j] = this.alphabet[altRandom];
+                   }
+                   altString = new String(altChars);
+               }
+               newString = altString;
+            }
+            randomStrings.add(newString);
         }
 
         for (String randomString : randomStrings) {
             theDLB.add(randomString);
-            assertEquals(theDLB.contains(randomString), true);
+            assertEquals(theDLB.searchPrefix(randomString), IS_WORD_ONLY);
         }
     }
 
     @Test
     public void testRandomizedWordInsertionCount() {
         DLB theDLB = new DLB();
-
         Random rnd = new Random();
         rnd.setSeed(0);
         String[] randomStrings = new String[this.WORD_COUNT];
@@ -104,7 +120,24 @@ public class DLBTest {
         //assertEquals(theDLB.size(), this.WORD_COUNT);
     }
 
+    @Test
+    public void testSearchPrefix() {
+        DLB theDLB = new DLB();
+        for (String word : testWords) {
+            theDLB.add(word);
+        }
 
+        for (String word : testWords) {
+            System.out.println("Testing word: " + word);
+
+            assertEquals(theDLB.searchPrefix(word), IS_WORD_ONLY);
+        }
+    }
+
+    @Test
+    public void testSearchPrefixWithBounds() {
+
+    }
 
     @Test
     public void testDictionaryPerformance() {
