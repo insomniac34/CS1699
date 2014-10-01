9a3e8ae29965c3c3d091b158efb47e1ff4f62beb
rushinjgr <rushinjgr@gmail.com>
2014-10-01 19:52:27 -0400

    added several mock/stub tests

--- LinkedListTest.java
+++ LinkedListTest.java
@@ -7,12 +7,8 @@ import static org.mockito.Mockito.when;
 public class LinkedListTest {
 
     @Test
-    public void testInsert() throws Exception {
-
-    }
-
-    @Test
-    //mock a node with value a
+    //mock a node with value 'a', set to the head of ll
+    //see if ll contains 'a'
     public void testContainsHead() throws Exception {
         Node mockNode = mock(Node.class);
         when(mockNode.getData()).thenReturn('a');
@@ -21,6 +17,9 @@ public class LinkedListTest {
         assertTrue(ll.contains('a'));
     }
 
+    @Test
+    //mock a node with value 'a', insert to ll
+    //see if ll contains 'a'
     public void testContainsInsert() throws Exception {
         Node mockNode = mock(Node.class);
         when(mockNode.getData()).thenReturn('a');
@@ -29,4 +28,25 @@ public class LinkedListTest {
         assertTrue(ll.contains('a'));
     }
 
+    @Test
+    //mock a node with value 'a', insert to ll
+    //see if ll contains 'a'
+    public void testContainsInsertFalse() throws Exception {
+        Node mockNode = mock(Node.class);
+        when(mockNode.getData()).thenReturn('b');
+        LinkedList ll = new LinkedList();
+        ll.insert(mockNode);
+        assertFalse(ll.contains('z'));
+    }
+
+    @Test
+    //mock a node with value 'a', set to the head of ll
+    //see if ll contains 'A'
+    public void testContainsHeadCaseSensitive() throws Exception {
+        Node mockNode = mock(Node.class);
+        when(mockNode.getData()).thenReturn('a');
+        LinkedList ll = new LinkedList();
+        ll.head = mockNode;
+        assertTrue(ll.contains('A'));
+    }
 }
