cd31745e0d3aeb3fca53cdf3ce52b4ea6d1875d7
rushinjgr <rushinjgr@gmail.com>
2014-10-01 19:45:57 -0400

    created Linked List Tests

--- LinkedListTest.java
+++ LinkedListTest.java
@@ -1,6 +1,8 @@
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class LinkedListTest {
 
@@ -10,7 +12,21 @@ public class LinkedListTest {
     }
 
     @Test
-    public void testContains() throws Exception {
-
+    //mock a node with value a
+    public void testContainsHead() throws Exception {
+        Node mockNode = mock(Node.class);
+        when(mockNode.getData()).thenReturn('a');
+        LinkedList ll = new LinkedList();
+        ll.head = mockNode;
+        assertTrue(ll.contains('a'));
     }
+
+    public void testContainsInsert() throws Exception {
+        Node mockNode = mock(Node.class);
+        when(mockNode.getData()).thenReturn('a');
+        LinkedList ll = new LinkedList();
+        ll.insert(mockNode);
+        assertTrue(ll.contains('a'));
+    }
+
 }
