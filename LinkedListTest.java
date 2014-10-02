ba6f584416eb7910ec1822ff50237b21112beab6
Tyler <insomniac34@gmail.com>
2014-10-01 20:06:52 -0400

    Added two tests to LinkedList

--- LinkedListTest.java
+++ LinkedListTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 public class LinkedListTest {
 
+    private final char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'};
+
     @Test
     public void testInsert() throws Exception {
 
@@ -13,4 +15,33 @@ public class LinkedListTest {
     public void testContains() throws Exception {
 
     }
+
+    /*
+    *  Asserts that the linked list grows linearly with nodes added
+    * */
+    @Test
+    public void testLinkedListGrowth() {
+        LinkedList linkedList = new LinkedList();
+        int llSize = 0;
+        for (char letter : alphabet) {
+            linkedList.insert(letter);
+            assertEquals(linkedList.size, llSize);
+            llSize++;
+        }
+    }
+
+    /*
+    *  Asserts that the most recently added node is ALWAYS the head of the Linked List
+    * */
+    @Test
+    public void testHeadAsMostRecentlyAddedNode() {
+        LinkedList linkedList = new LinkedList();
+        for (char letter : alphabet) {
+            Node node = new Node(letter);
+            linkedList.insert(node);
+            assertEquals(linkedList.head, node);
+        }
+    }
 }
+
+
